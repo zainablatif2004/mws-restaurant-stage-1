c9419951d07290fcf20d0317d363ca9aaa1d7fff
Zainab Latif <zainab_latif2004@yahoo.com>
2019-06-19 22:06:45 -0500

    Added a service worker

---
+++ sw.js
@@ -0,0 +1,46 @@
+var CACHE_NAME = 'restaurant-cache-v1';
+var urlsToCache = [
+  '/',
+  'css/styles.css',
+  'data/restaurants.json',
+  'img/1.jpg',
+  'img/2.jpg',
+  'img/3.jpg',
+  'img/4.jpg',
+  'img/5.jpg',
+  'img/6.jpg',
+  'img/7.jpg',
+  'img/8.jpg',
+  'img/9.jpg',
+  'img/10.jpg',
+  'js/dbhelper.js',
+  'js/main.js',
+  'js/restaurant_info.js',
+  'sw.js',
+  'index.html',
+  'restaurant.html'
+];
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});

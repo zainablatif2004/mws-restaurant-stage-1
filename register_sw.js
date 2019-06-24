411ea6e3fb42ffe090a79c83c49f7feadeb87e39
Zainab Latif <zainab_latif2004@yahoo.com>
2019-06-23 20:19:57 -0500

    Fixed the styles css and added more accessibility features.

--- register_sw.js
+++ register_sw.js
@@ -1,11 +1,9 @@
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('sw.js').then(function (registration) {
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            console.log('ServiceWorker registration failed: ', err);
+        });
     });
-  });
 }
